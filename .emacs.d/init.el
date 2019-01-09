90a45b288706fb44b2f9a015582a053dd13c94b1
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-09 10:53:36 +0100

    adds some icons to emacs and tweaks the spacemacs theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -88,12 +88,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(custom-safe-themes
    (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "73c69e346ec1cb3d1508c2447f6518a6e582851792a8c0e57a22d6b9948071b4" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+    ("8d805143f2c71cfad5207155234089729bb742a1cb67b7f60357fdd952044315" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "73c69e346ec1cb3d1508c2447f6518a6e582851792a8c0e57a22d6b9948071b4" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (key-chord anti-zenburn-theme ## spacemacs-theme counsel swiper swipe evil which-key use-package general avy))))
+    (spacegray-theme doom-themes key-chord anti-zenburn-theme ## spacemacs-theme counsel swiper swipe evil which-key use-package general avy)))
+ '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e")))
+ '(spacemacs-theme-comment-bg nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
