a8f6bced7873ab6d5b45aca679b33cadaaa168d7
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-09 10:33:47 +0100

    version control all the emacs configs and plugins instead of just the init.el

---
+++ .emacs.d/elpa/general-20181229.1542/general-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "general" "20181229.1542" "Convenience wrappers for keybindings." '((emacs "24.4") (cl-lib "0.5")) :commit "675050199b5a30d54a24b58a367db32c0bdc47f5" :keywords '("vim" "evil" "leader" "keybindings" "keys") :authors '(("Fox Kiester" . "noct@openmailbox.org")) :maintainer '("Fox Kiester" . "noct@openmailbox.org") :url "https://github.com/noctuid/general.el")
