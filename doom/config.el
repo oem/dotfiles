068a17eb7475d7d8bd3db165dc7a4bf660cdb23f
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-10 08:08:36 +0100

    adds a big font for big mode/emacs

--- doom/config.el
+++ doom/config.el
@@ -1,9 +1,13 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+;;; Code:
 (setq doom-font (font-spec :family "Tamsyn" :size 20))
+(setq doom-big-font (font-spec :family "Cartograph Mono CF Heavy" :size 26))
+(setq doom-theme 'doom-Iosvkem)
 (toggle-frame-fullscreen)
+
 ;; exit insert mode by pressing jj
 (setq-default evil-escape-key-sequence "jj")
 (setq-default evil-escape-delay 0.2)
-(setq doom-theme 'doom-Iosvkem)
+;;; config.el ends here
