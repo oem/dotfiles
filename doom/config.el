bbe5fe9b14a85977be72d80afd6cb4c239da8423
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-10 11:31:52 +0100

    removes scrollbar and tweaks some other emacs configs

--- doom/config.el
+++ doom/config.el
@@ -6,6 +6,8 @@
 (setq doom-big-font (font-spec :family "Cartograph Mono CF Heavy" :size 26))
 (setq doom-theme 'doom-Iosvkem)
 (toggle-frame-fullscreen)
+(global-visual-line-mode)
+(toggle-scroll-bar -1)
 
 ;; exit insert mode by pressing jj
 (setq-default evil-escape-key-sequence "jj")
