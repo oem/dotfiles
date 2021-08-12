af169b0f269440f02b962a4929b4b9e529e74640
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-08-12 08:45:25 +0200

    adds a few org mode configurations and other things (still just experimenting with settings)

--- emacs/init.el
+++ emacs/init.el
@@ -7,22 +7,26 @@
 (set-fringe-mode 15) ; Some breathing room
 (menu-bar-mode -1) ; Disable menu bar
 
-(setq ring-bell-function 'ignore) ; no thank you
-
+(setq visible-bell nil)
+(setq ring-bell-function #'ignore) ; no thank you
 
 ;; UI
 (set-face-attribute 'default nil :font "PragmataPro Mono" :height 140 :weight 'normal)
 (setq-default line-spacing 10)
-(load-theme 'wombat)
+
+(set-face-attribute 'fixed-pitch nil :font "PragmataPro Mono" :weight 'normal :height 140)
+(set-face-attribute 'variable-pitch nil :font "Avenir Next LT Pro" :weight 'regular :height 150)
+
 (toggle-frame-maximized)
+
 
 ;; Initialize package sources
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -56,20 +60,152 @@
   :config
   (ivy-mode 1))
 
+(use-package ivy-rich)
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom (doom-modeline-height 35))
 
+(use-package doom-themes
+  :init (load-theme 'doom-dark+ t))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package general
+  :config
+  (general-create-definer oem/leader-key-def
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (oem/leader-key-def
+    "b" '(:ignore t :which-key "buffer")
+    "bb" '(switch-to-buffer :which-key "switch buffer")))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package key-chord
+  :init
+  (setq key-chord-two-keys-delay 0.5)
+  (key-chord-define evil-insert-state-map "fd" 'evil-normal-state)
+  :config
+  (key-chord-mode 1))
+
+(use-package hydra)
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(oem/leader-key-def
+  "ts" '(hydra-text-scale/body :which-text "text scale"))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-callable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package all-the-icons)
+
+;; org mode
+(defun oem/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(use-package org
+  :hook (org-mode . oem/org-mode-setup)
+  :config
+  (setq org-ellipsis " ✜"))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◎" "◉" "○" "●")))
+
+(require 'org-indent)
+
+(dolist (face '((org-level-1 . 2.6)
+		(org-level-2 . 2.0)
+		(org-level-3 . 1.6)
+		(org-level-4 . 1.4)
+		(org-level-5 . 1.2)
+		(org-level-6 . 1.1)
+		(org-level-7 . 1.1)
+		(org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Avenir Next LT Pro" :weight 'bold :height (cdr face)))
+
+;; we don't want variable fonts for everything in org mode:
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+(defun oem/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . oem/org-mode-visual-fill))
+
+
 ;; key bindings
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline swiper ivy use-package)))
+ '(custom-safe-themes
+   '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
+ '(package-selected-packages
+   '(visual-fill-column visual-fill visual-fill-mode org-indent org-bullets hydra vterm ivy-rich key-chord evil-collection which-key use-package helpful general evil doom-themes doom-modeline counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
