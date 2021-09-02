7ce8b17ffdb79066e409c86dfea66706e2340d31
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-02 13:25:23 +0200

    adds python lsp settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -197,6 +197,9 @@ nvim_lsp.solargraph.setup({})
 -- Enable typescript/javascript
 nvim_lsp.tsserver.setup{}
 
+--  Enable python/pyright
+require'lspconfig'.pyright.setup{}
+
 -- Enable diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 vim.lsp.diagnostic.on_publish_diagnostics, {
