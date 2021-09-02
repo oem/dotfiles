f3dba1706a869876042e85dfaa00b3d8826bbe38
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-02 11:04:32 +0200

    hihglights current indentation level

--- nvim/init.lua
+++ nvim/init.lua
@@ -142,7 +142,11 @@ require('packer').startup(function()
 	use {
 		'lukas-reineke/indent-blankline.nvim',
 		config = function()
-			require('indent_blankline').setup{ show_end_of_line = true, space_char_blankline = " " }
+			require('indent_blankline').setup{
+				show_end_of_line = true,
+				space_char_blankline = " ",
+				show_current_context = true,
+			}
 		end
 	}
 
