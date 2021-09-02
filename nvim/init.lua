f357eff149f8a117c8c517a49b43cd8756e28af8
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-02 20:46:32 +0200

    tweaking the scrolling, still not convinced I like it

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,7 +154,7 @@ require('packer').startup(function()
 				use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
 				respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
 				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-				easing_function = nil,       -- Default easing function
+				easing_function = 'sine',    -- Default easing function
 				pre_hook = nil,              -- Function to run before the scrolling animation starts
 				post_hook = nil,             -- Function to run after the scrolling animation ends
 			})
