b2a2699ebdbdf1e9c9976cd026741b5765efe4da
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-02 11:01:06 +0200

    shows indentation levels

--- nvim/init.lua
+++ nvim/init.lua
@@ -138,6 +138,13 @@ require('packer').startup(function()
   -- Pretty symbols
   use 'kyazdani42/nvim-web-devicons'
 
+	-- show indentation levels
+	use {
+		'lukas-reineke/indent-blankline.nvim',
+		config = function()
+			require('indent_blankline').setup{ show_end_of_line = true, space_char_blankline = " " }
+		end
+	}
   -- Statusline
   use {
     'famiu/feline.nvim',
@@ -165,6 +172,10 @@ o.backspace = "indent,eol,start"
 o.shell = "/bin/bash" -- remain posix compatible, even when using fish otherwise
 o.background = "dark"
 o.updatetime=300
+vim.opt.listchars = {
+	space = "⋅",
+	eol = "↴",
+}
 
 -- window-local options
 wo.number = false
