91b7d88b6323edaa24b79bb23d3de49d750e26ac
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-11 10:08:45 +0200

    a few tweaks here and there, mostly browser defaults

--- emacs/init.el
+++ emacs/init.el
@@ -456,3 +456,16 @@
   "m" '(:ignore t :which-key "mail")
   "mm" '(mu4e :which-key "mu4e")
   "ms" '(mu4e-headers-search :which-key "mail search"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,4 +1,19 @@
+from qutebrowser.api import interceptor
+
+def filter_yt(info: interceptor.Request):
+    """Block the given request if necessary."""
+    url = info.request_url
+    if (url.host() == 'www.youtube.com' and
+            url.path() == '/get_video_info' and
+            '&adformat=' in url.query()):
+        info.block()
+
+interceptor.register(filter_yt)
+
 config.load_autoconfig()
 config.bind(',m', 'spawn mpv {url}')
 config.bind(',M', 'hint links spawn mpv {hint-url}')
-config.set("colors.webpage.darkmode.enabled", True)
+config.set("fonts.tabs.selected",'14pt default_family')
+config.set("fonts.tabs.unselected",'14pt default_family')
+config.set("fonts.default_size",'14pt')
+config.set("fonts.web.size.default_fixed", 16)
