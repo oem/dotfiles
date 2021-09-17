ef4136d7736a1a9063f575f79b93cb299a534acd
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-17 23:41:57 +0200

    changes agenda file dir to use what emacs is also using

--- nvim/lua/config/orgmode.lua
+++ nvim/lua/config/orgmode.lua
@@ -1,5 +1,5 @@
 require('orgmode').setup({
-	org_agenda_files = ('~/sync/notes/projects/**/*'),
+	org_agenda_files = ('~/sync/notes/**/*'),
 	org_default_notes_file = '~/sync/notes/notes.org',
 	org_todo_keywords = { 'TODO(t)', 'NEXT(n)', '|', 'DONE(d)' },
 	org_hide_leading_stars = true,
