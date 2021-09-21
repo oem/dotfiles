a5cf58df61879c66fe71889aa47b9bb8efb4f99d
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-21 23:17:53 +0200

    adds some improved telescope bindings

--- neovim/.config/nvim/lua/config/telescope_setup.lua
+++ neovim/.config/nvim/lua/config/telescope_setup.lua
@@ -1,5 +1,6 @@
 local map = require('config.utils').map
 local silent = { silent = true }
+local noremap = { noremap = true }
 
 map('n', '<leader>F', [[<cmd>Telescope find_files theme=get_dropdown<cr>]], silent)
 map('n', '<leader>f', [[<cmd>Telescope git_files theme=get_dropdown<cr>]], silent)
@@ -8,6 +9,8 @@ map('n', '<leader>s', [[<cmd>Telescope live_grep theme=get_dropdown<cr>]], silen
 map('n', '<leader>le', [[<cmd>Telescope lsp_workspace_diagnostics theme=get_dropdown<cr>]], silent)
 map('n', '<leader>t', [[<cmd>Telescope treesitter theme=get_dropdown<cr>]], silent)
 map('n', '<leader>b', [[<cmd>Telescope buffers show_all_buffers=true theme=get_dropdown<cr>]], silent)
-map('n', '<leader>lr', '<cmd>Telescope lsp_references<cr>')
-map('n', '<leader>ld', '<cmd>Telescope lsp_definitions<cr>')
-map('n', '<leader>li', '<cmd>Telescope lsp_implementations<cr>')
+map('n', '<leader>lr', [[<cmd>Telescope lsp_references theme=get_dropdown<cr>]], noremap)
+map('n', '<leader>ld', [[<cmd>Telescope lsp_definitions theme=get_dropdown<cr>]], noremap)
+map('n', '<leader>li', [[<cmd>Telescope lsp_implementations theme=get_dropdown<cr>]], noremap)
+map('n', '<leader>ps', [[<cmd>Telescope grep_string theme=get_dropdown<cr>]], noremap)
+map('n', '<leader>df', [[<cmd>Telescope grep_string theme=get_dropdown<cr>]], noremap)
