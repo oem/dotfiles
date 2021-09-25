b775cd83c483979f47a2cb03271f0fb808c98c92
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-25 16:58:28 +0200

    silently curl for cht.sh

--- launch/cht.sh
+++ launch/cht.sh
@@ -7,7 +7,7 @@ read -p "query: "  query
 
 if printf "$languages" | grep -qs "$selected"; then
 	query=`echo $query|tr ' ' '+'`
-	tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+	tmux neww bash -c "curl -s cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-	tmux neww bash -c "curl cht.sh/$selected~$query|bat"
+	tmux neww bash -c "curl -s cht.sh/$selected~$query|bat"
 fi
