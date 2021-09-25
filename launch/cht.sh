170aa8c8b078fbd6160233fd6194215b208926e9
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-09-25 16:11:20 +0200

    adds a little wrapper (shamelessly stolen from primeagen) for cht.sh

---
+++ launch/cht.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+languages=`echo "golang rust lua javascript nodejs ruby html css haskell elm" | tr ' ' '\n'`
+core_utils=`echo "xargs find mv sed awk ps" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+read -p "query: "  query
+
+if printf "$languages" | grep -qs "$selected"; then
+	query=`echo $query|tr ' ' '+'`
+	tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+	tmux neww bash -c "curl cht.sh/$selected~$query|bat"
+fi
--- neovim/.config/nvim/plugin/packer_compiled.lua
+++ neovim/.config/nvim/plugin/packer_compiled.lua
@@ -109,7 +109,7 @@ _G.packer_plugins = {
     path = "/home/oem/.local/share/nvim/site/pack/packer/opt/cmp-path"
   },
   cmp_luasnip = {
-    after_files = { "/home/oem/.local/share/nvim/site/pack/packer/opt/cmp_luasnip/after/plugin/cmp_luasnip.lua" },
+    after_files = { "/home/oem/.local/share/nvim/site/pack/packer/opt/cmp_luasnip/after/plugin/cmp_luasnip.lua", "/home/oem/.local/share/nvim/site/pack/packer/opt/cmp_luasnip/after/plugin/cmp_luasnip.vim" },
     load_after = {
       ["nvim-cmp"] = true
     },
