3c5070599204c9dbeb41c3329c5d1cd93e03d4ed
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-03 19:22:30 +0200

    a little bit of brainstorming with the goal of stepping back a bit and exploring the build in functionality of emacs instead of replacing everying with packages

--- emacs/.emacs.gnu/init.el
+++ emacs/.emacs.gnu/init.el
@@ -167,7 +167,7 @@
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
+         ;; ("C-x C-f" . counsel-find-file)
          :map minibuffer-local-map
          ("C-r" . 'counsel-minibuffer-history)))
 
