817a0231589445c1f8807adb093e9df074a59d20
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-06 11:48:48 +0200

    vim/telescope: allows live grepping hidden files too

--- neovim/.config/nvim/lua/config/telescope.lua
+++ neovim/.config/nvim/lua/config/telescope.lua
@@ -3,6 +3,16 @@ local telescope = require 'telescope'
 
 telescope.setup {
   defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden"
+    },
     mappings = {
       i = {
         ["<esc>"] = actions.close
