2f4d1c8d44e5639eabd41e4546d91864e91f4aa5
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-08 10:02:34 +0200

    minor fixes to git keybindings

--- neovim/.config/nvim/lua/config/neogit.lua
+++ neovim/.config/nvim/lua/config/neogit.lua
@@ -1,6 +1,9 @@
 local map = require('config.utils').map
-local silent = { silent = true }
+local options = {
+    silent = true,
+    noremap = true
+}
 
-map('n', '<leader>gg', [[<cmd>Neogit<cr>]], silent)
-map('n', '<leader>gb', [[<cmd>Git blame<cr>]], silent)
+map('n', '<leader>gg', [[<cmd>Neogit<cr>]], options)
+map('n', '<leader>gb', [[<cmd>Git blame<cr>]], options)
 
