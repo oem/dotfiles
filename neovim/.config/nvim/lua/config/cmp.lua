77147f34023cddaca0581f2124b77a8874aeff92
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-10 15:58:08 +0200

    simplifies cmp nvim mappings

--- neovim/.config/nvim/lua/config/cmp.lua
+++ neovim/.config/nvim/lua/config/cmp.lua
@@ -1,49 +1,15 @@
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 
-local function check_backspace()
-    local col = vim.fn.col '.' - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
-end
-
-local feedkeys = vim.fn.feedkeys
-local pumvisible = vim.fn.pumvisible
-local replace_termcodes = vim.api.nvim_replace_termcodes
-local next_item_keys = replace_termcodes('<c-n>', true, true, true)
-local prev_item_keys = replace_termcodes('<c-p>', true, true, true)
-local backspace_keys = replace_termcodes('<tab>', true, true, true)
-local snippet_next_keys = replace_termcodes('<plug>luasnip-expand-or-jump',
-                                            true, true, true)
-local snippet_prev_keys = replace_termcodes('<plug>luasnip-jump-prev', true,
-                                            true, true)
-
 cmp.setup {
     snippet = {
         expand = function(args) luasnip.lsp_expand(args.body) end
     },
     mapping = {
         ['<cr>'] = cmp.mapping.confirm(),
-        ['<tab>'] = cmp.mapping(function(fallback)
-            if pumvisible() == 1 then
-                feedkeys(next_item_keys, 'n')
-            elseif luasnip.expand_or_jumpable() then
-                feedkeys(snippet_next_keys, '')
-            elseif check_backspace() then
-                feedkeys(backspace_keys, 'n')
-            else
-                fallback()
-            end
-        end, {'i', 's'})
+        ['<tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
+        ['<s-tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})
     },
-    ['<s-tab>'] = cmp.mapping(function(fallback)
-        if pumvisible() == 1 then
-            feedkeys(prev_item_keys, 'n')
-        elseif luasnip.jumpable(-1) then
-            feedkeys(snippet_prev_keys, '')
-        else
-            fallback()
-        end
-    end, {'i', 's'}),
     sources = {
         {
             name = 'buffer'
