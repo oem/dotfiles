826c258854b227a04083261a56547d1808624d93
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-10 08:30:07 +0200

    minor cosmetic tweaks to note search

--- neovim/.config/nvim/lua/oem/telescope.lua
+++ neovim/.config/nvim/lua/oem/telescope.lua
@@ -1,14 +1,14 @@
 local M = {}
 M.find_notes = function()
     require('telescope.builtin').find_files {
-        prompt_title = 'notes>',
+        prompt_title = '<notes::files>',
         cwd = '~/sync/notes'
     }
 end
 
 M.search_notes = function()
     require('telescope.builtin').live_grep {
-        prompt_title = 'notes>',
+        prompt_title = '<notes::search>',
         cwd = '~/sync/notes'
     }
 end
