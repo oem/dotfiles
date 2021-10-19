c56f400bdd665bf3edfbffcec8b20f1f67c777f0
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-19 18:58:02 +0200

    removes unused old vim plugins (that shouldn't have been checked in in the first place)

--- vim/ct.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/.vim/bundle/command-t/ruby/command-t
-RBENV_VERSION=system ruby extconf.rb
-make
--- vim/ftplugin/javascript.vim
+++
@@ -1,4 +0,0 @@
-imap <leader><cr> <esc>A;<esc>o
-setlocal expandtab
-setlocal shiftwidth=2
-setlocal softtabstop=2
