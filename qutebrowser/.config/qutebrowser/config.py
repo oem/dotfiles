27657bcd6685a153c42d167d339b90c1d7ae1e15
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-11-08 21:10:21 +0100

    improves configuration of qutebrowser (like, readable fixed font for example)

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,21 +1,24 @@
 from qutebrowser.api import interceptor
 
+
 def filter_yt(info: interceptor.Request):
     """Block the given request if necessary."""
     url = info.request_url
-    if (url.host() == 'www.youtube.com' and
-            url.path() == '/get_video_info' and
-            '&adformat=' in url.query()):
+    if (
+        url.host() == "www.youtube.com"
+        and url.path() == "/get_video_info"
+        and "&adformat=" in url.query()
+    ):
         info.block()
+
 
 interceptor.register(filter_yt)
 
 config.load_autoconfig()
-config.bind(',m', 'spawn mpv {url}')
-config.bind(',M', 'hint links spawn mpv {hint-url}')
-config.set("fonts.tabs.selected",'16pt default_family')
-config.set("fonts.tabs.unselected",'16pt default_family')
-config.set("fonts.default_size",'16pt')
-config.set("fonts.web.size.default", 16)
-config.set("fonts.web.size.default_fixed", 16)
-config.set("fonts.default_family", "TerminessTTF Nerd Font")
+config.bind(",m", "spawn mpv {url}")
+config.bind(",M", "hint links spawn mpv {hint-url}")
+
+c.zoom.default = "150%"
+c.fonts.default_size = "16pt"
+c.fonts.default_family = ["Cartograph CF Medium"]
+c.fonts.web.size.default_fixed = 16
