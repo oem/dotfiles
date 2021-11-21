45249acdb569954ee9e3dbe3bf54cfd4167fbd17
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-11-21 09:09:37 +0100

    removes deprecates cr mapping for autopairs

--- neovim/.config/nvim/lua/config/autopairs.lua
+++ neovim/.config/nvim/lua/config/autopairs.lua
@@ -2,8 +2,3 @@ local npairs = require('nvim-autopairs')
 npairs.setup({})
 npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
 
-require('nvim-autopairs.completion.cmp').setup({
-    map_cr = true,
-    insert = false
-})
-
