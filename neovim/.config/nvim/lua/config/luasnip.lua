d30664ffd3c47eb9cb9f484356023c4b39c8d31e
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-11-22 13:45:56 +0100

    starting to move out the luasnip config into its own file

---
+++ neovim/.config/nvim/lua/config/luasnip.lua
@@ -0,0 +1,8 @@
+require'luasnip'.config.setup({})
+vim.cmd [[
+  imap <silent><expr> <c-k> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<c-k>'
+  inoremap <silent> <c-j> <cmd>lua require('luasnip').jump(-1)<CR>
+  imap <silent><expr> <C-l> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-l>'
+  snoremap <silent> <c-k> <cmd>lua require('luasnip').jump(1)<CR>
+  snoremap <silent> <c-j> <cmd>lua require('luasnip').jump(-1)<CR>
+]]
