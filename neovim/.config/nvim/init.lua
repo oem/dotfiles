42dd44380ef0edf6f263dcfe989b6da0030f3787
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-12-02 17:45:25 +0100

    only try to show inline hints for rust files

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -154,7 +154,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
 
 -- enable inline hints
 autocmd(nil,
-        'CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost * lua require\'lsp_extensions\'.inlay_hints{ prefix = "", highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }',
+        'CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *.rs lua require\'lsp_extensions\'.inlay_hints{ prefix = "", highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }',
         nil)
 
 -- code navigation
