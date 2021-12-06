ef43257b20955874b7f73ac326afe35cc56c054a
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-12-06 16:39:44 +0100

    reorders the cmp sources to priorities snippets over buffer

--- neovim/.config/nvim/lua/config/cmp.lua
+++ neovim/.config/nvim/lua/config/cmp.lua
@@ -3,28 +3,15 @@ local luasnip = require 'luasnip'
 local lspkind = require 'lspkind'
 
 cmp.setup {
-    formatting = {
-        format = lspkind.cmp_format()
-    },
-    snippet = {
-        expand = function(args) luasnip.lsp_expand(args.body) end
-    },
+    formatting = {format = lspkind.cmp_format()},
+    snippet = {expand = function(args) luasnip.lsp_expand(args.body) end},
     mapping = {
         ['<cr>'] = cmp.mapping.confirm(),
         ['<tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
         ['<s-tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})
     },
     sources = {
-        {
-            name = 'nvim_lsp'
-        }, {
-            name = 'buffer'
-        }, {
-            name = 'nvim_lua'
-        }, {
-            name = 'path'
-        }, {
-            name = 'luasnip'
-        }
+        {name = 'luasnip'}, {name = 'nvim_lsp'}, {name = 'buffer'},
+        {name = 'nvim_lua'}, {name = 'path'}
     }
 }
