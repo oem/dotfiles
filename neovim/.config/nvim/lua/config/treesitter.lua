43471326f631a6a07033424f3b3088177376017b
Ömür Özkir <oemuer.oezkir@gmail.com>
2022-05-27 11:16:36 +0200

    fixes treesitter setup

--- neovim/.config/nvim/lua/config/treesitter.lua
+++ neovim/.config/nvim/lua/config/treesitter.lua
@@ -1,28 +1,27 @@
--- require'nvim-treesitter.configs'.setup({
---     playground = {
---         enable = true
---     },
---     query_linter = {
---         enable = true,
---         use_virtual_text = true,
---         lint_events = {"BufWrite", "CursorHold"}
---     },
---     ensure_installed = "maintained",
---     highlight = {
---         enable = true
---     },
---     autopairs = {
---         enable = true
---     },
---     refactor = {
---         highlight_definitions = {
---             enable = true
---         },
---         smart_rename = {
---             enable = true,
---             keymaps = {
---                 smart_rename = "grr"
---             }
---         }
---     }
--- })
+require'nvim-treesitter.configs'.setup({
+    playground = {
+        enable = true
+    },
+    query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = {"BufWrite", "CursorHold"}
+    },
+    highlight = {
+        enable = true
+    },
+    autopairs = {
+        enable = true
+    },
+    refactor = {
+        highlight_definitions = {
+            enable = true
+        },
+        smart_rename = {
+            enable = true,
+            keymaps = {
+                smart_rename = "grr"
+            }
+        }
+    }
+})
