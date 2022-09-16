8766a9976c39792e3c744972dea8a972d12c4848
Ömür Özkir <oemuer.oezkir@gmail.com>
2022-09-16 16:05:12 +0200

    switches telescope usage to lua function calls - seems like the Telescope command is not available anymore(?)

--- neovim/.config/nvim/lua/config/telescope_setup.lua
+++ neovim/.config/nvim/lua/config/telescope_setup.lua
@@ -2,25 +2,17 @@ local map = require('config.utils').map
 local options = {silent = true, noremap = true}
 
 -- files
-map('n', '<leader>ff',
-    [[<cmd>Telescope find_files theme=get_dropdown hidden=true<cr>]], options)
-map('n', '<leader>fF', [[<cmd>Telescope git_files theme=get_dropdown<cr>]],
-    options)
-map('n', '<leader>fr', [[<cmd>Telescope frecency theme=get_dropdown<cr>]],
-    options)
+map('n', '<leader>ff', ":lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown())<cr>", options)
+map('n', '<leader>fF', ":lua require('telescope.builtin').git_files(require('telescope.themes').get_dropdown())<cr>", options)
+map('n', '<leader>fr', ":lua require('telescope').extensions.frecency.frecency(require('telescope.themes').get_dropdown())<cr>", options)
 
 -- buffers
-map('n', '<leader>bb',
-    [[<cmd>Telescope buffers show_all_buffers=true theme=get_dropdown<cr>]],
-    options)
+map('n', '<leader>bb', ":lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown())<cr>", options)
 
 -- search
-map('n', '<leader>ss', [[<cmd>Telescope live_grep theme=get_dropdown<cr>]],
-    options)
-map('n', '<leader>sb',
-    [[<cmd>Telescope current_buffer_fuzzy_find theme=get_dropdown<cr>]], options)
-map('n', '<leader>sw', [[<cmd>Telescope grep_string theme=get_dropdown<cr>]],
-    options)
+map('n', '<leader>ss', ":lua require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown())<cr>", options)
+map('n', '<leader>sb', ":lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown())<cr>", options)
+map('n', '<leader>sw', ":lua require('telescope.builtin').grep_string(require('telescope.themes').get_dropdown())<cr>", options)
 
 -- notes
 -- requires ripgrep and fd
@@ -29,18 +21,13 @@ map('n', '<leader>ns', ":lua require('oem.telescope').search_notes()<cr>",
 map('n', '<leader>nf', ":lua require('oem.telescope').find_notes()<cr>", options)
 
 -- help
-map('n', '<leader>hh', [[<cmd>Telescope help_tags<cr>]], options)
+map('n', '<leader>hh', ":lua require('telescope.builtin').help_tags(require('telescope.themes').get_dropdown())<cr>", options)
 
 -- LSP
-map('n', '<leader>le', [[<cmd>Telescope diagnostics theme=get_dropdown<cr>]],
-    options)
-map('n', '<leader>t', [[<cmd>Telescope treesitter theme=get_dropdown<cr>]],
-    options)
-map('n', '<leader>lr', [[<cmd>Telescope lsp_references theme=get_dropdown<cr>]],
-    options)
-map('n', '<leader>ld',
-    [[<cmd>Telescope lsp_definitions theme=get_dropdown<cr>]], options)
-map('n', '<leader>li',
-    [[<cmd>Telescope lsp_implementations theme=get_dropdown<cr>]], options)
-map('n', '<leader>la',
-    [[<cmd>Telescope lsp_range_code_actions theme=get_dropdown<cr>]], options)
+map('n', '<leader>le', ":lua require('telescope.builtin').diagnostics(require('telescope.themes').get_dropdown())<cr>", options)
+map('n', '<leader>lr', ":lua require('telescope.builtin').lsp_references(require('telescope.themes').get_dropdown())<cr>", options)
+map('n', '<leader>ld', ":lua require('telescope.builtin').lsp_definitions(require('telescope.themes').get_dropdown())<cr>", options)
+map('n', '<leader>li', ":lua require('telescope.builtin').lsp_implementations(require('telescope.themes').get_dropdown())<cr>", options)
+
+-- treesitter
+map('n', '<leader>t', ":lua require('telescope.builtin').treesitter(require('telescope.themes').get_dropdown())<cr>", options)
