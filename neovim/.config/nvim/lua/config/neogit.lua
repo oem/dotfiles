b126d4d9138d7f257dc3a6b152c22d97afa39bd1
Ömür Özkir <oemuer.oezkir@gmail.com>
2022-09-28 15:10:47 +0200

    let's stick with the previous keybinds for now

--- neovim/.config/nvim/lua/config/neogit.lua
+++ neovim/.config/nvim/lua/config/neogit.lua
@@ -1,6 +1,6 @@
 local map = require('config.utils').map
 local options = {silent = true, noremap = true}
 
-map('n', '<leader>vv', [[<cmd>Neogit<cr>]], options)
-map('n', '<leader>vb', [[<cmd>Git blame<cr>]], options)
+map('n', '<leader>gg', [[<cmd>Neogit<cr>]], options)
+map('n', '<leader>gb', [[<cmd>Git blame<cr>]], options)
 
