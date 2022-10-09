69f54e165606ac85b663b80443e78e6877977f95
Ömür Özkir <oemuer.oezkir@gmail.com>
2022-10-09 15:01:12 +0200

    adds treesitter object keybinds for various things

--- neovim/.config/nvim/lua/config/treesitter.lua
+++ neovim/.config/nvim/lua/config/treesitter.lua
@@ -1,27 +1,37 @@
 require'nvim-treesitter.configs'.setup({
-    playground = {
-        enable = true
+    textobjects = {
+        select = {
+            enable = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner"
+            }
+        },
+        move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+                ["]f"] = "@function.outer",
+                ["]c"] = "@class.outer"
+            },
+            goto_previous_start = {
+                ["[f"] = "@function.outer",
+                ["[c"] = "@class.outer"
+            }
+        }
     },
+    playground = {enable = true},
     query_linter = {
         enable = true,
         use_virtual_text = true,
         lint_events = {"BufWrite", "CursorHold"}
     },
-    highlight = {
-        enable = true
-    },
-    autopairs = {
-        enable = true
-    },
+    highlight = {enable = true},
+    autopairs = {enable = true},
     refactor = {
-        highlight_definitions = {
-            enable = true
-        },
-        smart_rename = {
-            enable = true,
-            keymaps = {
-                smart_rename = "grr"
-            }
-        }
+        highlight_definitions = {enable = true},
+        smart_rename = {enable = true, keymaps = {smart_rename = "grr"}}
     }
 })
