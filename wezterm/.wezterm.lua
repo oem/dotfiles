6c2e585bdba13c70bc1c9aa123f48990f208a4f2
Ömür Özkir <oemuer.oezkir@gmail.com>
2023-07-24 13:04:07 +0200

    no need to custom make the wez term theme - there is already a premade one

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -13,32 +13,7 @@ end
 config.font = wezterm.font { family = "Berkeley Mono", weight = "Bold" }
 config.font_size = 15.0
 
--- colors
-config.colors = {
-    foreground = '#c0caf5',
-    background = '#24283b',
-    ansi = {
-        '#1D202F',
-        '#f7768e',
-        '#9ece6a',
-        '#e0af68',
-        '#7aa2f7',
-        '#bb9af7',
-        '#7dcfff',
-        '#a9b1d6',
-    },
-    brights = {
-        '#414868',
-        '#f7768e',
-        '#9ece6a',
-        '#e0af68',
-        '#7aa2f7',
-        '#bb9af7',
-        '#7dcfff',
-        '#c0caf5',
-    },
-    cursor_bg = 'orange',
-}
+config.color_scheme = "Tokyo Night Storm"
 
 -- keys
 config.keys = {
