a1cb468137f9a5c7bbaa7a04e25c7fd351f0d863
Ömür Özkir <oemuer.oezkir@gmail.com>
2024-01-29 15:47:42 +0100

    fixes gitsigns config #neovim

--- neovim/.config/nvim/lua/config/gitsigns.lua
+++ neovim/.config/nvim/lua/config/gitsigns.lua
@@ -1,14 +1,14 @@
+local map = require('config.utils').map
+
 require('gitsigns').setup {
-  signs = {
-    add = { hl = 'GreenSign', text = '│', numhl = 'GitSignsAddNr' },
-    change = { hl = 'BlueSign', text = '│', numhl = 'GitSignsChangeNr' },
-    delete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
-    topdelete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
-    changedelete = { hl = 'PurpleSign', text = '│', numhl = 'GitSignsChangeNr' },
-  },
-	signcolumn = true,
-  keymaps = {
-		['n ]g'] = '<cmd>lua require"gitsigns".next_hunk()<cr>',
-		['n [g'] = '<cmd>lua require"gitsigns".prev_hunk()<cr>',
-	},
+    signs = {
+        add = { hl = 'GreenSign', text = '│', numhl = 'GitSignsAddNr' },
+        change = { hl = 'BlueSign', text = '│', numhl = 'GitSignsChangeNr' },
+        delete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
+        topdelete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
+        changedelete = { hl = 'PurpleSign', text = '│', numhl = 'GitSignsChangeNr' },
+    },
+    signcolumn = true,
+    map('n', ']g', '<cmd>lua require"gitsigns".next_hunk()<cr>'),
+    map('n', '[g', '<cmd>lua require"gitsigns"._prev_hunk()<cr>')
 }
