0c99b1294ca47af85a38002dc104d7f086906627
Ömür Özkir <oemuer.oezkir@gmail.com>
2024-08-13 20:41:41 +0200

    removes old gitsigns config

--- neovim/.config/nvim/lua/config/gitsigns.lua
+++ neovim/.config/nvim/lua/config/gitsigns.lua
@@ -1,11 +1,4 @@
 require('gitsigns').setup {
-    signs = {
-        add = { hl = 'GreenSign', text = '│', numhl = 'GitSignsAddNr' },
-        change = { hl = 'BlueSign', text = '│', numhl = 'GitSignsChangeNr' },
-        delete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
-        topdelete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
-        changedelete = { hl = 'PurpleSign', text = '│', numhl = 'GitSignsChangeNr' },
-    },
     signcolumn = true,
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
