170348deab41d16a7d84140f9aa2ddd7344f2416
Ömür Özkir <oemuer.oezkir@gmail.com>
2025-06-29 16:39:45 +0200

    minor tweaks to my terminal ui

--- neovim/.config/nvim/lua/config/statusline.lua
+++ neovim/.config/nvim/lua/config/statusline.lua
@@ -30,7 +30,7 @@ basic.vi_mode = {
     name = 'vi_mode',
     hl_colors = colors_mode,
     text = function()
-        return { { '  ', state.mode[2] } }
+        return { { ' ', state.mode[2] } }
     end,
 }
 basic.square_mode = {
